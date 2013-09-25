a0c6a3c1a351df4bc4a463a19de604b1d9248305
Barrett Clark <barrett@websitesbybarrett.com>
2013-09-25 09:59:35 -0500

    Add a mood list UITableView page

--- app/controllers/mood_list_controller.rb
+++ app/controllers/mood_list_controller.rb
@@ -1,15 +1,10 @@
-class RootController < UITableViewController
-  FACES = {
-    smile: '0x1F600',
-    neutral: '0x1F610',
-    frown: '0x1F622'
-  }
-  CELLID = "face"
+class MoodListController < UITableViewController
+  CELLID = 'face'
 
   def viewDidLoad
     super
     view.dataSource = view.delegate = self
-    navigationItem.title = "Pick A Mood"
+    navigationItem.title = "Your Moods"
   end
 
   # def tableView(tableView, titleForHeaderInSection:section)
@@ -22,7 +17,7 @@ class RootController < UITableViewController
   # end
 
   def tableView(tableView, numberOfRowsInSection:section)
-    FACES.keys.count
+    1
   end
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
@@ -32,27 +27,16 @@ class RootController < UITableViewController
       cell
     end
 
-    cell.textLabel.text = face_emoji(indexPath.row) + " #{face_name(indexPath.row)}"
+    cell.textLabel.text = "You picked a face"
     cell
   end
 
   def tableView(tableView, didSelectRowAtIndexPath:indexPath)
-    tableView.deselectRowAtIndexPath(indexPath, animated: true)
-
-    @mood_list_controller ||= MoodListController.alloc.init
-    @mood_list_controller.selected_face(face(indexPath.row))
-    self.navigationController.pushViewController(@mood_list_controller, animated:true)
   end
 
-  private
+  # Helper methods
 
-  def face(index)
-    face = FACES.to_a[index]
-  end
-  def face_emoji(index)
-    face(index)[1].hex.chr(Encoding::UTF_8)
-  end
-  def face_name(index)
-    face(index)[0].capitalize
+  def selected_face(face)
+    @face = face
   end
 end
